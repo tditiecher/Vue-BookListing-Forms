25287c2fb3b1e4f14d0c513240009cc69e96e919
Taco D <taco@tdev.org>
2020-11-29 12:41:47 +0100

    module2

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,11 +1,12 @@
 <template>
   <div>
-    <h1>{{title}}</h1>
+    <h1>{{ title }}</h1>
     <ul>
-      <book-item v-for='book in books' :key='book.id' :book='book'></book-item>
+      <book-item v-for="book in books" :key="book.id" :book="book"></book-item>
     </ul>
-    <br><hr>
-    <book-form @addBook='appendBook'></book-form>
+    <br />
+    <hr />
+    <book-form @addBook="appendBook"></book-form>
   </div>
 </template>
 
@@ -20,21 +21,37 @@ export default {
       title: "All Books",
       states: ["Want to Read", "Read", "Reading"],
       books: [
-        { title: "Self-Reliance", author: "Ralph Waldo Emerson" },
-        { title: "American Gods", author: "Neil Gaiman" },
-        { title: "Amusing Ourselves to Death", author: "Neil Postman" }
-      ]
+        {
+          title: "Self-Reliance",
+          author: "Ralph Waldo Emerson",
+          finishedReading: true,
+        },
+        {
+          title: "American Gods",
+          author: "Neil Gaiman",
+          finishedReading: false,
+        },
+        {
+          title: "Amusing Ourselves to Death",
+          author: "Neil Postman",
+          finishedReading: true,
+        },
+      ],
     };
   },
   components: {
     BookItem,
-    BookForm
+    BookForm,
   },
   methods: {
-    appendBook(bookTitle, bookAuthor) {
-      this.books.push({ title: bookTitle, author: bookAuthor });
-    }
-  }
+    appendBook(bookData) {
+      this.books.push({
+        title: bookData.bookTitle,
+        author: bookData.bookAuthor,
+        finishedReading: bookData.finishedReading,
+      });
+    },
+  },
 };
 </script>
 
